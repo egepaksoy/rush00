061fe1bdbd83cf6ae7fc564cb90fa96a60a8c14f
Ege Paksoy <egepaskoy@gmail.com>
2023-02-05 08:43:24 +0300

    rushindex 1 den baslayip xe kadar gidiyor

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -6,7 +6,7 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 14:42:35 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/02/04 15:18:02 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/02/05 08:41:58 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,17 @@ void	rush(int x, int y)
 	int	var_y;
 	int	var_x;
 
-	x--;
-	y--;
-	var_y = 0;
+	var_y = 1;
 	while (var_y <= y)
 	{
-		var_x = 0;
+		var_x = 1;
 		while (var_x <= x)
 		{
-			if ((var_x == 0 || var_x == x) && (var_y == 0 || var_y == y))
+			if ((var_x == 1 || var_x == x) && (var_y == 1 || var_y == y))
 				ft_putchar('o');
-			else if (var_x < x && (var_y == 0 || var_y == y))
+			else if (var_x < x && (var_y == 1 || var_y == y))
 				ft_putchar('-');
-			else if ((var_x == 0 || var_x == x) && var_y < y)
+			else if ((var_x == 1 || var_x == x) && var_y < y)
 				ft_putchar('|');
 			else
 				ft_putchar(' ');
